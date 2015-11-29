8a65e6cc8025114e542c518115d32f4dbeacc319
rladeira <rwladeira@gmail.com>
2015-11-29 19:42:38 -0200

    Add new dataset: cuboids.

---
+++ dataParsing/syntheticDatasets/cuboids/parseCuboids.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+
+library(rDatasets)
+library(mlbench)
+
+datasetPath <- file.path("dataParsing", "syntheticDatasets", "cuboids")
+
+mlBenchCuboids <- mlbench.cuboids(5000, relsize=c(3,3,3,1))
+plot(mlBenchCuboids)
+
+cuboidsData <- as.data.frame(mlBenchCuboids)
+
+cuboidsData <- data.frame(cuboidsData[,1:3],
+                         sapply(1:7, function(i) runif(5000)),
+                         cuboidsData[,4])
+colnames(cuboidsData) <- c(paste("F", 1:10, sep = ""), "class")
+
+
+cuboids_ <- buildDataAbstraction(
+  name = "Cuboids",
+  data = cuboidsData[, -11],
+  labels = cuboidsData[, 11])
+
+save(cuboids_,
+     file = file.path(
+       datasetPath, "cuboids_.RData"))
