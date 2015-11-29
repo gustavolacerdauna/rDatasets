b5f71cc99d1df33abeb13ae28bbd0524fb386c34
rladeira <rwladeira@gmail.com>
2015-11-29 19:55:37 -0200

    Add new dataset: ringnorm

---
+++ dataParsing/syntheticDatasets/ringnorm/parseRingnorm.R
@@ -0,0 +1,26 @@
+rm(list = ls())
+
+library(rDatasets)
+library(mlbench)
+
+datasetPath <- file.path("dataParsing", "syntheticDatasets", "ringnorm")
+
+mlBenchRingnorm <- p<-mlbench.ringnorm(1000, d=2)
+plot(mlBenchRingnorm)
+
+ringnormData <- as.data.frame(mlBenchRingnorm)
+
+ringnormData <- data.frame(ringnormData[,1:2],
+                           sapply(1:8, function(i) runif(1000)),
+                           ringnormData[,3])
+colnames(ringnormData) <- c(paste("F", 1:10, sep = ""), "class")
+
+
+ringnorm_ <- buildDataAbstraction(
+  name = "Ringnorm",
+  data = ringnormData[, -11],
+  labels = ringnormData[, 11])
+
+save(ringnorm_,
+     file = file.path(
+       datasetPath, "ringnorm_.RData"))
