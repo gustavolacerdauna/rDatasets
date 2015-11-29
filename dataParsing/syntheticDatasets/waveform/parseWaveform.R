4a6945064d6d7b6fc142d2e3493a8bb55b4f1ce7
rladeira <rwladeira@gmail.com>
2015-11-29 20:46:56 -0200

    Add two new datasets: waveform and ozone.

---
+++ dataParsing/syntheticDatasets/waveform/parseWaveform.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+
+library(rDatasets)
+library(mlbench)
+
+datasetPath <- file.path("dataParsing", "syntheticDatasets", "waveform")
+
+mlBenchWaveform <- mlbench.waveform(1000)
+
+waveformData <- as.data.frame(mlBenchWaveform)
+
+waveformData <- data.frame(waveformData[,-22],
+                           sapply(1:29, function(i) runif(1000)),
+                           waveformData[,22])
+colnames(waveformData) <- c(paste("F", 1:50, sep = ""), "class")
+
+
+waveform_ <- buildDataAbstraction(
+  name = "Waveform",
+  data = waveformData[, -11],
+  labels = waveformData[, 11])
+
+save(waveform_,
+     file = file.path(
+       datasetPath, "waveform_.RData"))
