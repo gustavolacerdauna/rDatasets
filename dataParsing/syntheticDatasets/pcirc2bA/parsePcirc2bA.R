c540702873e1ddcdcb215d43e6ee487b31889652
rladeira <rwladeira@gmail.com>
2015-11-29 23:13:11 -0200

    reparse pcirc2bA to unify its feature names.

--- dataParsing/syntheticDatasets/pcirc2bA/parsePcirc2bA.R
+++ dataParsing/syntheticDatasets/pcirc2bA/parsePcirc2bA.R
@@ -7,6 +7,8 @@ datasetPath <- file.path("dataParsing", "syntheticDatasets", "pcirc2bA")
 
 pcirc2bA <- readMat(file.path(datasetPath, "pcirc2bA.mat"))
 
+colnames(pcirc2bA$Xo) <- paste("F", 1:10, sep = "")
+
 pcirc2bA_ <- buildDataAbstraction(
   name = "Pcirc2bA",
   data = pcirc2bA$Xo,
